7f4d07472996cf2876a15a9c432a60373894b6de
sean-dooher <sean@dooher.net>
2018-04-20 18:13:35 -0700

    Add router to app.jsx

--- stanford/frontend/assets/js/app.jsx
+++ stanford/frontend/assets/js/app.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import MainDashboardApi from "./containers/MainDashboardApi"
 import 'whatwg-fetch';
+import {NavBar} from "./components/NavBar";
+import history from "./history";
 
 export class App extends React.Component {
     render() {
-        return <MainDashboardApi />;
+        return (<div>
+                    <NavBar>
+                        <Router history={history}>
+                            <Route exact={true} path="/">
+                                <MainDashboardApi />
+                            </Route>
+                        </Router>
+                    </NavBar>
+                </div>);
     }
 }
