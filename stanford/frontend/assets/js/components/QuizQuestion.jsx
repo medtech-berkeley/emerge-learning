8eac86b95312bb03f4975a78bfc5d56b7fe19bef
Ian Galbraith <igalbraith27@gmail.com>
2018-07-28 14:39:48 -0500

    Sean said this would work

--- stanford/frontend/assets/js/components/QuizQuestion.jsx
+++ stanford/frontend/assets/js/components/QuizQuestion.jsx
@@ -22,24 +22,24 @@ export class QuizQuestion extends React.Component {
 
 								{/*	If the user is done and this is the correct answer we want to make it green. */}
 								{this.props.done && this.props.correct_answers.includes(answer.id) &&
-									<div className="card-body" outline color ="success" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
+									<Card body outline color="success" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
 										<p className="card-text">{answer.text}</p>
-									</div>
+									</Card>
 								}
 								{/*	If the user is done and they selected an incorrect answer we want to make it red. */}
 								{this.props.done && this.props.selected === answer.id && !this.props.correct_answers.includes(answer.id) &&
-									<div className="card-body" outline color ="danger" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
+									<Card body outline color="danger" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
 										<p className="card-text">{answer.text}</p>
-									</div>
+									</Card>
 								}
 								{/*We want to just render the rest of the answers:
 									done and not correct and not selected
 									not done render normally
 									*/}
 									{this.props.done && this.props.correct_answers.includes(answer.id) && this.props.selected !== answer.id &&
-										<div className="card-body" outline color ="success" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
+										<Card body outline color="success" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
 											<p className="card-text">{answer.text}</p>
-										</div>
+										</Card>
 									}
 									{!this.props.done &&
 										<div className="card-body" onClick={() => this.props.answerQuestion(this.props.id, answer.id, this.props.categoryId)}>
