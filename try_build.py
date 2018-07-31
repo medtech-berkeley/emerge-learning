7a0ed6ec734dff2931d74385def725a05c3ee8f7
Sean Dooher <sean@dooher.net>
2018-07-30 21:12:08 -0700

    Remove testing config from build

--- try_build.py
+++ try_build.py
@@ -3,10 +3,10 @@ import os
 import sys
 import subprocess
 
-if os.system('docker-compose -p %s -f docker-compose.yml -f testing.yml build' % os.environ['ID']) == 1:
+if os.system('docker-compose -p %s -f docker-compose.yml build' % os.environ['ID']) == 1:
 	sys.exit(1)
 
-up_proc = subprocess.Popen(['docker-compose', '-p', os.environ['ID'], '-f', 'docker-compose.yml', '-f' 'testing.yml', 'run', 'interfaceserver'], stdout=subprocess.PIPE)
+up_proc = subprocess.Popen(['docker-compose', '-p', os.environ['ID'], '-f', 'docker-compose.yml', 'run', 'interfaceserver'], stdout=subprocess.PIPE)
 
 while True:
 	up_proc.poll()
@@ -15,8 +15,8 @@ while True:
 	line = up_proc.stdout.readline()
 	print(line)
 	if str(line, 'utf-8').startswith("Quit the server with CONTROL-C."):
-		os.system('docker-compose -f docker-compose.yml -f testing.yml down')
+		os.system('docker-compose -f docker-compose.yml down')
 		sys.exit(0)
 
-os.system('docker-compose -f docker-compose.yml -f testing.yml down')
+os.system('docker-compose -f docker-compose.yml down')
 sys.exit(1)
