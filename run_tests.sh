727197c6a6b76f57a4fb349fb7dc67c5c5f22dab
Sean Dooher <sean@dooher.net>
2018-07-30 22:27:37 -0700

    Increase timeout on waiting for db

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dockerize -wait tcp://database:5432
+dockerize -wait tcp://database:5432 -timeout 20s
 
 python3 manage.py migrate
 python3 manage.py jenkins --enable-coverage --no-input
