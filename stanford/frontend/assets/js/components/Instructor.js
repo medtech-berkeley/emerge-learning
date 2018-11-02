780d870738e4fa515c61a3dfc20d2910ba203b0b
Ethan Teo <ethanteo99@berkeley.edu>
2018-11-02 15:07:57 -0700

    configure basic instructor frontend

---
+++ stanford/frontend/assets/js/components/Instructor.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Button, Jumbotron } from 'reactstrap';
+
+export class Instructor extends React.Component {
+
+    render() {
+        return ( 
+            <div>
+                <Jumbotron>
+                <h1 className = "display-3" > Welcome to a Terrible Instructor JS file </h1> 
+                <p className = "lead" > This is a simple mock up. </p> 
+                <hr className = "my-2" />
+                <p> Two file - upload links are below. </p> 
+                <p className = "lead">
+                    <Button outline color = "success" size = "sm">
+                        <form action = "/" >
+                            <input type = "file" name = "fileone" accept = ".csv" />
+                            <input type = "submit" />
+                        </form>
+                    </Button> 
+                    {   } 
+                    <Button outline color = "success" size = "sm" >
+                        <form action = "/">
+                            <input type = "file" name = "filetwo" accept = ".csv" />
+                            <input type = "submit" />
+                        </form>
+                    </Button>  
+                </p>
+                </Jumbotron>
+                <br/>
+            </div>
+        )
+    }
+}
