05e72f4c7a50529bde8da57f9c9589a618e40529
Sean Dooher <sean@dooher.net>
2019-02-14 23:08:17 -0800

    Switch to docker mailserv

--- stanford/stanford/settings.py
+++ stanford/stanford/settings.py
@@ -217,9 +217,9 @@ else:
 # Email
 
 EMAIL_USE_TLS = True,
-EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST = 'mailserv'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
