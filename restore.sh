c2a245fc1d9fda333242701b4748e2916d10da9a
Sean Dooher <sean@dooher.net>
2019-02-17 10:28:55 +0000

    Fix restore script

--- restore.sh
+++ restore.sh
@@ -6,7 +6,7 @@ mkdir -p $folder
 
 gzip -dc $folder/db_data.gz | docker exec -i stanford_database_1 psql -U postgres
 
-docker run --rm --volumes-from stanford-nginx -v $folder:/backup ubuntu tar -C /media_files -xvf /backup/media.tar
+docker run --rm --volumes-from stanford-nginx -v $folder:/backup ubuntu tar -C / -xvf /backup/media.tar
 
-docker run --rm --volumes-from stanford-nginx -v $folder:/backup ubuntu tar -C /static -xvf /backup/static.tar
+docker run --rm --volumes-from stanford-nginx -v $folder:/backup ubuntu tar -C / -xvf /backup/static.tar
 
