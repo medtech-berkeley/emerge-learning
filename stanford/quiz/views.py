319568f2c86a26bd2461910ea72971523e42172d
Sean Dooher <sean@dooher.net>
2019-06-21 20:13:29 -0700

    Properly hides out of date quizzes

--- stanford/quiz/views.py
+++ stanford/quiz/views.py
@@ -85,7 +85,7 @@ class QuizViewSet(ModelViewSet):
 
     def get_queryset(self):
         in_time = Quiz.objects.filter(start__lte=timezone.now(), end__gte=timezone.now())        
-        quiz_uds = QuizUserData.objects.filter(student=self.request.student)
+        quiz_uds = QuizUserData.objects.filter(student=self.request.user.student)
         completed = Quiz.objects.filter(id__in=quiz_uds.values('quiz_id'))
 
         return in_time | completed
