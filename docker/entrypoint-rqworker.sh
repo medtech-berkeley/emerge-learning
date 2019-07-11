3606a4c81f8c02f02672128e28b3e9d6047ba8b8
Sean Dooher <sean@dooher.net>
2019-07-10 19:47:55 -0700

    Add basic redis queue docker file setup

--- docker/entrypoint-rqworker.sh
+++ docker/entrypoint-rqworker.sh
@@ -7,6 +7,4 @@ until PGPASSWORD=$POSTGRES_PASSWORD psql -h "database" -U "$POSTGRES_NAME" -c '\
 	    sleep 1
 done
 
-python manage.py migrate
-python manage.py collectstatic --noinput
-exec python manage.py runserver 0.0.0.0:8000
+exec python manage.py rqworker default high low
--- stanford/stanford/settings.py
+++ stanford/stanford/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'quiz',
     'frontend',
+    "django_rq",
     'accounts'
 ]
 
